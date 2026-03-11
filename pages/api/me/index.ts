05572e5ae57136a53db2cec13cb8c5e2f590654f
Pablo Kachuk <kachuck_p665@hotmail.com>
2026-03-11 04:38:44 -0300

    7° Update(P221): Un console.log()

--- pages/api/me/index.ts
+++ pages/api/me/index.ts
@@ -1,4 +1,5 @@
 export default function (req,res){
-    const MiName:string="Pablo";
+    const MiName:string="© 2026 - (КП) : [Pablo Kachuk]";
+    console.log("Soy la API de: ",MiName);
     res.send(MiName);
 };
